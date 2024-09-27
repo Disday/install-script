c94e66d30f0b9855986a902f31682c01f0391b06
disdain44@yandex.ru <disdain44@yandex.ru>
2024-09-27 15:55:43 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿string[] compList = ["comp1", "comp2", "comp3"];
+Installer.Install(compList);
+
+
+class Logger(string logPath = "log.log")
+{
+    public void Log(string message, string compName)
+    {
+        string entry = $"{DateTime.Now} - {compName} - {message}\n";
+        File.AppendAllText(logPath, entry);
+    }
+}
+
+class Installer
+{
+    public static void Install(string[] compList)
+    {
+        Logger logger = new("new.log");
+
+        foreach (var compName in compList)
+        {
+            logger.Log("some text", compName);
+        }
+    }
+}
